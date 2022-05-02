67f369a561ec8ff433034a8cf7452d407a8593f4
salihkilicoglu <salihkilicoglu@gmail.com>
2022-05-02 22:45:54 +0300

    validation and superuser

--- app/core/models.py
+++ app/core/models.py
@@ -7,8 +7,19 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Create and save new user"""
+        if not email:
+            raise ValueError('Email address can not be empty')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Creates and saves a new super user"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
 
         return user
