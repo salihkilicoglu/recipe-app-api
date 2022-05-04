b5eea28275ee9d7c5082f09bb4151bd6b085ed00
salihkilicoglu <salihkilicoglu@gmail.com>
2022-05-04 16:08:18 +0300

    api/user/create/ endpoint

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -8,6 +8,7 @@ from rest_framework import status
 
 CREATE_USER_URL = reverse('user:create')
 
+
 def create_user(**params):
     return get_user_model().objects.create_user(**params)
 
@@ -36,7 +37,8 @@ class PublicUserApiTests(TestCase):
         """Test creating a user that already exists fails"""
         payload = {
             'email': 'test@gmail.com',
-            'password': 'testpass'
+            'password': 'testpass',
+            'name': 'Test'
         }
         create_user(**payload)
 
@@ -46,8 +48,8 @@ class PublicUserApiTests(TestCase):
 
     def test_password_too_short(self):
         """Test that password must be more than 5 chars"""
-        payload = {'email': 'test@gmail.com', 'password': 'pw'}
-        res = res.client.post(CREATE_USER_URL, payload)
+        payload = {'email': 'test@gmail.com', 'password': 'pw', 'name': 'Test'}
+        res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
         user_exists = get_user_model().objects.filter(
